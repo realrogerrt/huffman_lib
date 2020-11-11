7e9f18bcbc02ff17fd6beea6f127a6bdb11f3640
Roger Rodriguez Texido <rogerkael@gmail.com>
2020-11-11 10:08:16 -0500

    Setup Config
    Setup namespace and libray.

--- src/core.cc
+++ src/core.cc
@@ -1,14 +1,26 @@
-// Formatting library for C++
-//
-// Copyright (c) 2012 - 2016, Victor Zverovich
-// All rights reserved.
-//
-// For the license information refer to format.h.
-
 #include "huffman/core.h"
 
 namespace huffman {
-  void foo() {
-      std::cout << "This is foo" << std::endl;
+    std::ostream& operator << (std::ostream& os, const __frequency_node& n)
+    {
+      return os << "value=" << n.__value << " " << "frequence=" << n.__frequency;
+    }
+
+    std::ifstream& operator >> (std::ifstream& is, uint8_t& n)
+    {
+      char c;
+      is >> c;
+      n = static_cast<uint8_t>(c);
+      return is;
+    }
+
+    std::ifstream& operator >> (std::ifstream& is, __frequency_node& n)
+    {
+      char ch;
+      is >> ch;
+      // is >> n.__value;
+      n.__value = static_cast<uint8_t>(ch);
+      n.__frequency = 1;
+      return is;
     }
 }
