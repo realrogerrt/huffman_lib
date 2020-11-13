823b9ae9907abfb38a4c10f989521d48375e8987
Roger Rodriguez Texido <rogerkael@gmail.com>
2020-11-12 22:52:18 -0500

    Basic
    Using regular pointers.

--- include/huffman/core.h
+++ include/huffman/core.h
@@ -15,16 +15,17 @@ namespace huffman {
 using namespace std;
 struct __frequency_node {
 
-  shared_ptr<__frequency_node> __parent;
-  shared_ptr<__frequency_node> __left;
-  shared_ptr<__frequency_node> __right;
+  __frequency_node* __parent;
+  __frequency_node* __left;
+  __frequency_node* __right;
 
   uint8_t __value;
   uint32_t __frequency;
 
-  shared_ptr<__frequency_node> __merge(__frequency_node&);
+  __frequency_node* __merge(__frequency_node*);
 
 };
+typedef shared_ptr<__frequency_node> node_ptr;
 
 bool operator<(const __frequency_node&, const __frequency_node&);
 
--- src/core.cc
+++ src/core.cc
@@ -6,26 +6,23 @@ bool operator<(const __frequency_node& a, const __frequency_node& b) {
   return a.__frequency < b.__frequency;
 }
 
+__frequency_node* __frequency_node::__merge(
+    __frequency_node* other) {
 
-shared_ptr<__frequency_node> __frequency_node::__merge(
-    __frequency_node& other) {
+  __frequency_node* n = new __frequency_node;
+  n->__frequency = this->__frequency + other->__frequency;
 
-  __frequency_node n; // = {0, }
-  n.__frequency = this->__frequency + other.__frequency;
-  shared_ptr<__frequency_node> p(&n);
+  n->__left = this;
+  n->__right = other;
 
-  p->__left = shared_ptr<__frequency_node>(this);
-  p->__right = shared_ptr<__frequency_node>(&other);
+  other->__parent = n;
+  this->__parent = n;
 
-  other.__parent = p;
-  this->__parent = p;
-
-
-  return p;
+  return n;
 }
 
 std::ostream& operator<<(std::ostream& os, const __frequency_node& n) {
-  return os << "value=" << n.__value << " " << "frequency=" << n.__frequency << endl;
+  return os << "value=" << n.__value << " " << "frequency=" << n.__frequency;
 }
 
 std::ifstream& operator>>(std::ifstream& is, __frequency_node& n) {
